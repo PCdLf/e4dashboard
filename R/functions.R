2fd148709d851bf5c96c756112a26d08be03bec0
Remko Duursma <remko@shintolabs.nl>
2021-06-15 16:13:21 +0200

    summary table in report

--- R/functions.R
+++ R/functions.R
@@ -39,4 +39,22 @@ logo_image_with_link <- function(img_path, url){
 }
 
 
+analysis_summary_table <- function(a){
+  
+  r <- range(a$data$EDA$DateTime)
+  time_range <- as.numeric(difftime(r[2], r[1], units = "min"))
+  
+  tibble::tribble(~Parameter, ~Value,
+                  "Mean acceleration", mean(a$data$ACC$a, na.rm=TRUE),
+                  "Mean temperature", mean(a$data$TEMP$TEMP, na.rm=TRUE),
+                  "Mean heartrate", mean(a$data$HR$HR, na.rm=TRUE),
+                  "Nr. accepted beats", a$ibi$summary$beats$beats_accepted,
+                  "Nr. original beats", a$ibi$summary$beats$beats_original,
+                  "rMSSD", a$ibi$time_analysis$rMSSD,
+                  "Number of peaks per minute", nrow(a$eda_peaks) / time_range,
+                  "Mean area under the curve (AUC)", mean(a$eda_peaks$AUC, na.rm = TRUE),
+                  "% of data with EDA artefacts", 100 * mean(a$eda_bin$label == -1)
+  )
+}
+
 
