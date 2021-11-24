4b7bf0c7ddb903626be482b5cf44336208a0f8f1
PCdLf <peter_de_looff@hotmail.com>
2021-11-24 09:08:44 +0100

    Inserted a path to Pandoc for a windows computer. Note that the folder containing pandoc might vary between devices and users.

--- server.R
+++ server.R
@@ -11,6 +11,10 @@ function(input, output, session) {
 
   rendered_plots <- callModule(visualizationModule, "viz", data = data_in, calendar = calendar)
   
+  # Rmarkdown could not find pandoc and thus has to be set before calling the downloadhandler
+  # Please note that this could be a different path depending on your computer.
+  Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
+  
   callModule(analysisModule, "analysis", data = data_in, plots = rendered_plots,
              calendar = calendar)
  
